e8f12b42689d3dbec69b8691f7aaee9300b97ce2
Charlie <69505003+charliedodds@users.noreply.github.com>
2024-01-19 11:14:50 +0000

    Set up gql api and show tasks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,53 @@
-const Home = () => {
+import { Task } from '@/__generated__/graphql'
+import createApolloClient from '@/api'
+import { ApolloError, gql } from '@apollo/client'
+import { FC } from 'react'
+
+const TASKS = gql(`
+  query GetTasks {
+    taskList {
+      _id
+      title
+    }
+  }
+`)
+
+interface Props {
+  data: { taskList: Task[] }
+  loading: boolean
+  error: ApolloError | null
+}
+
+const Home: FC<Props> = ({ data, loading, error }) => {
+  if (loading) return 'Loading...'
+
+  if (error) return `Error ${error.name}: ${error.message}`
+
   return (
     <main>
-      <h1>Hello world!</h1>
+      <ul>
+        {data.taskList.map((task) => (
+          <li key={task._id}>{task.title}</li>
+        ))}
+      </ul>
     </main>
   )
 }
 
 export default Home
+
+export async function getStaticProps() {
+  const client = createApolloClient()
+
+  const { data, loading, error } = await client.query<Task[]>({
+    query: TASKS,
+  })
+
+  return {
+    props: {
+      data,
+      loading,
+      error: error ?? null,
+    },
+  }
+}
